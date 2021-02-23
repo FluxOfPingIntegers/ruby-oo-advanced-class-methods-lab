04a9315b00ced73c2f683850de544847fa2982cd
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-02-23 10:34:08 -0500

    commit tree ff0b2b3468bae755cb9a9fa938b78d618abbcce4

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -8,6 +9,49 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    @@all << self.new
+    @@all[-1]
+  end
+
+  def self.new_by_name(name)
+    self.create.name = "#{name}"
+    @@all[-1]
+  end
+
+  def self.create_by_name(name)
+    self.new_by_name(name)
+  end
+
+  def self.find_by_name(name)
+    Song.all.find {|songs| songs.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+   self.create_by_name(name)
+   self.find_by_name(name)
+  end
+
+  def self.alphabetical
+    @@all.sort_by {|i| i.name}
+  end
+
+  def self.new_from_filename(filename)
+    filename = filename.split(" - ")
+    filename[1] = filename[1].delete_suffix(".mp3")
+    x = self.find_or_create_by_name(filename[1])
+    x.artist_name=(filename[0])
+    x
+  end
+
+  def self.create_from_filename(filename)
+    self.new_from_filename(filename)
+  end
+
+  def self.destroy_all
+    @@all.clear
   end
 
 end
